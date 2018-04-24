e4cd956ec3b4a139e919a1eb5e6017c6c385f298
39Er <liuweimin@51talk.com>
2018-04-24 18:22:38 +0800

    add hashring

---
+++ cache/hashring.go
@@ -0,0 +1,109 @@
+package alg
+
+import (
+	"crypto/sha1"
+	"math"
+	"sort"
+	"strconv"
+)
+
+const (
+	DefaultVirualSpots = 400
+)
+
+type node struct {
+	nodeKey   string
+	spotValue uint32
+}
+
+type nodeArray []node
+
+func (p nodeArray) Len() int { return len(p) }
+
+func (p nodeArray) Less(i, j int) bool { return p[i].spotValue < p[j].spotValue }
+
+func (p nodeArray) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+
+func (p nodeArray) Sort() { sort.Sort(p) }
+
+type HashRing struct {
+	vituralSpots int
+	nodes        nodeArray
+	weights      map[string]int
+}
+
+func NewHashRing(spots ...int) *HashRing {
+	var sportNum int
+	if len(spots) == 0 {
+		sportNum = DefaultVirualSpots
+	} else {
+		sportNum = spots[0]
+	}
+	return &HashRing{
+		vituralSpots: sportNum,
+		weights:      make(map[string]int),
+	}
+}
+
+func (h *HashRing) AddNodes(nodeWeight map[string]int) {
+	for nodeKey, w := range nodeWeight {
+		h.weights[nodeKey] = w
+	}
+	h.generate()
+}
+
+func (h *HashRing) AddNode(nodeKey string, weight int) {
+	h.weights[nodeKey] = weight
+	h.generate()
+}
+
+func (h *HashRing) Delete(nodeKey string) {
+	delete(h.weights, nodeKey)
+	h.generate()
+}
+
+func (h *HashRing) generate() {
+	var totalW int
+	for _, w := range h.weights {
+		totalW += w
+	}
+	totalVirualSpots := h.vituralSpots * len(h.weights)
+	h.nodes = nodeArray{}
+	for nodeKey, w := range h.weights {
+		spots := int(math.Floor(float64(w) / float64(totalW) * float64(totalVirualSpots)))
+		for i := 1; i <= spots; i++ {
+			hash := sha1.New()
+			hash.Write([]byte(nodeKey + ":" + strconv.Itoa(i)))
+			hashBytes := hash.Sum(nil)
+			n := node{
+				nodeKey:   nodeKey,
+				spotValue: getValue(hashBytes[6:10]),
+			}
+			h.nodes = append(h.nodes, n)
+			hash.Reset()
+		}
+	}
+	h.nodes.Sort()
+}
+
+func getValue(bs []byte) uint32 {
+	if len(bs) < 4 {
+		return 0
+	}
+	return uint32(bs[3]<<24) | uint32(bs[2]<<16) | uint32(bs[1]<<8) | uint32(bs[0])
+}
+
+func (h *HashRing) GetNode(s string) string {
+	if len(h.nodes) == 0 {
+		return ""
+	}
+	hash := sha1.New()
+	hash.Write([]byte(s))
+	hashBytes := hash.Sum(nil)
+	v := getValue(hashBytes[6:10])
+	i := sort.Search(len(h.nodes), func(i int) bool { return h.nodes[i].spotValue >= v })
+	if i == len(h.nodes) {
+		i = 0
+	}
+	return h.nodes[i].nodeKey
+}
---
+++ cache/hashring_test.go
@@ -0,0 +1,25 @@
+package alg
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHashRing_AddNode(t *testing.T) {
+	hashring := NewHashRing()
+	hashring.AddNode("node0", 1)
+	nodes := make(map[string]int)
+	for i := 1; i < 5; i++ {
+		nodeKey := fmt.Sprintf("node%d", i)
+		nodes[nodeKey] = i
+	}
+	hashring.AddNodes(nodes)
+	for i := 0; i < 10; i++ {
+		fmt.Println(hashring.GetNode(string(i)))
+	}
+
+	fmt.Println("----------------------")
+	for i := 0; i < 10; i++ {
+		fmt.Println(hashring.GetNode(string(i)))
+	}
+}
